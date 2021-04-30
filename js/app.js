e1c7c99fbbfaabcf7a365d3ba3ba74fec91f93e2
Rhoann Mae Galicio <rhoannmaegalicio@gmail.com>
2021-04-30 14:28:49 +0800

    Validating Fields Part 2

--- js/app.js
+++ js/app.js
@@ -34,5 +34,32 @@ function validateField() {
     let errors;
 
     // Validate the length of the field
-    validateLength(this)
+    validateLength(this);
+
+    // Validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+// Validate the length of the fields
+function validateLength(field) {
+    if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+// validate email (checks for @ in the value)
+function validateEmail(field) {
+    let emailText = field.value;
+    // check if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
